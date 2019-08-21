0c7665a6de57f2bb52ce99ca17a1566d9adc6548
Alan19 <Alan19@users.noreply.github.com>
2019-08-20 23:56:04 -0400

    Fix Astral damage event handler

--- src/main/java/com/alan199921/astral/events/TravellingHandlers.java
+++ src/main/java/com/alan199921/astral/events/TravellingHandlers.java
@@ -16,23 +16,33 @@ import net.minecraftforge.fml.common.Mod;
 public class TravellingHandlers {
     @SubscribeEvent
     public static void doNotTargetAstrals(LivingSetAttackTargetEvent event) {
-        if (event.getTarget() != null && event.getTarget().isPotionActive(Effects.FIRE_RESISTANCE)) {
-            System.out.println(event.getEntityLiving().getName().getString() + " is no longer targetting you!");
-            MobHelper.resetTarget(event.getEntityLiving());
+        if (isAstralVsNonAstral(event.getTarget(), event.getEntityLiving())) {
+//            System.out.println(event.getEntityLiving().getName().getString() + " is no longer targetting you!");
+            event.getEntityLiving().setRevengeTarget(null);
         }
     }
 
     @SubscribeEvent
     public static void nullifyAstralDamage(LivingAttackEvent event) {
-        LivingEntity livingSource = (LivingEntity) event.getSource().getImmediateSource();
-
-        if (livingSource != null && isAstralVsNonAstral(livingSource, event.getEntityLiving())){
+        if (event.getSource().getTrueSource() != null && !event.getSource().getTrueSource().isLiving() && !event.getSource().getDamageType().equals("astral") && event.getEntityLiving().isPotionActive(Effects.FIRE_RESISTANCE)){
             event.setCanceled(true);
         }
+        if (event.getSource().getTrueSource() != null && event.getSource().getTrueSource().isLiving()){
+            LivingEntity livingSource = (LivingEntity) event.getSource().getImmediateSource();
+
+            if (isAstralVsNonAstral(livingSource, event.getEntityLiving())){
+                event.setCanceled(true);
+            }
+        }
+
+
     }
 
     //Function for detecting if an Astral entity is interacting with a non astral entity
     private static boolean isAstralVsNonAstral(LivingEntity mobA, LivingEntity mobB) {
+        if (mobA == null || mobB == null){
+            return false;
+        }
         Effect astralEffect = Effects.FIRE_RESISTANCE;
         return mobA.isPotionActive(astralEffect) && !mobB.isPotionActive(astralEffect) || !mobA.isPotionActive(astralEffect) && mobB.isPotionActive(astralEffect);
     }
@@ -40,7 +50,7 @@ public class TravellingHandlers {
     @SubscribeEvent
     public static void renderAstralEntities(RenderLivingEvent event) {
         if (!Minecraft.getInstance().player.isPotionActive(Effects.FIRE_RESISTANCE) && event.getEntity().isPotionActive(Effects.FIRE_RESISTANCE)) {
-            System.out.println("Making " + event.getEntity().getName().getString() + " invisible");
+            System.out.println("Making " + event.getEntity().getName().getString() + " invisible!");
             event.setCanceled(true);
         }
     }
