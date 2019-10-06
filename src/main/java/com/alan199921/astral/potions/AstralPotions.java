828fd92d2a57a9c0718f768fed71ce97de5b88e9
Alan19 <Alan19@users.noreply.github.com>
2019-10-06 16:50:27 -0400

    Switch to forge registries

--- src/main/java/com/alan199921/astral/potions/AstralPotions.java
+++ src/main/java/com/alan199921/astral/potions/AstralPotions.java
@@ -8,7 +8,7 @@ import net.minecraft.potion.Potion;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.IForgeRegistry;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class AstralPotions {
@@ -20,23 +20,25 @@ public class AstralPotions {
     @SubscribeEvent
     public static void onPotionRegistry(final RegistryEvent.Register<Potion> event) {
         int baseAstralTravelDuration = AstralConfig.getPotionEffectDurations().getAstralTravelDuration();
-        astralTravelPotion = registerPotion(AstralEffects.astralTravelEffect, "astral_travel_potion", baseAstralTravelDuration, 0);
-        longAstralTravelPotion = registerPotion(AstralEffects.astralTravelEffect, "long_astral_travel_potion", baseAstralTravelDuration * 2, 0);
-        strongAstralTravelPotion = registerPotion(AstralEffects.astralTravelEffect, "strong_astral_travel_potion", baseAstralTravelDuration / 2, 1);
+        astralTravelPotion = registerPotion(event.getRegistry(), AstralEffects.astralTravelEffect, "astral_travel_potion", baseAstralTravelDuration, 0);
+        longAstralTravelPotion = registerPotion(event.getRegistry(), AstralEffects.astralTravelEffect, "long_astral_travel_potion", baseAstralTravelDuration * 2, 0);
+        strongAstralTravelPotion = registerPotion(event.getRegistry(), AstralEffects.astralTravelEffect, "strong_astral_travel_potion", baseAstralTravelDuration / 2, 1);
     }
 
     /**
      * Function to create and register a potion with only one effect
      *
+     *
+     * @param eventRegistry
      * @param effect    What effect should the potion apply
      * @param name      The registry name of the potion
      * @param duration  The duration of the potion, in ticks
      * @param amplifier The level of the effect
      * @return A potion object
      */
-    public static Potion registerPotion(Effect effect, String name, int duration, int amplifier) {
+    public static Potion registerPotion(IForgeRegistry<Potion> eventRegistry, Effect effect, String name, int duration, int amplifier) {
         Potion potion = new Potion(new EffectInstance(effect, duration, amplifier)).setRegistryName(name);
-        ForgeRegistries.POTION_TYPES.register(potion);
+        eventRegistry.register(potion);
         return potion;
     }
 }
