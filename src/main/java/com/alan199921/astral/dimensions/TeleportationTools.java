cfe2d9cc0ce304cc43e3c78d853d9787be98431e
Alan19 <Alan19@users.noreply.github.com>
2020-01-10 12:19:30 -0500

    Send player their potion effects when teleporting to another dimension

--- src/main/java/com/alan199921/astral/dimensions/TeleportationTools.java
+++ src/main/java/com/alan199921/astral/dimensions/TeleportationTools.java
@@ -1,6 +1,8 @@
 package com.alan199921.astral.dimensions;
 
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.network.play.server.SPlayEntityEffectPacket;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.dimension.DimensionType;
 import net.minecraft.world.server.ServerWorld;
@@ -17,5 +19,9 @@ public class TeleportationTools {
         ServerWorld nextWorld = player.getServer().getWorld(type);
         nextWorld.getChunk(pos);    // make sure the chunk is loaded
         player.teleport(nextWorld, pos.getX(), pos.getY(), pos.getZ(), player.rotationYaw, player.rotationPitch);
+        for (EffectInstance effectinstance : player.getActivePotionEffects()) {
+            player.connection.sendPacket(new SPlayEntityEffectPacket(player.getEntityId(), effectinstance));
+        }
+
     }
 }
