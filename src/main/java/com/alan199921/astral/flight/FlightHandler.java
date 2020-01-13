faf941ca3d7b9019489ea2c63fd05789d84bb056
Alan19 <Alan19@users.noreply.github.com>
2020-01-12 20:23:12 -0500

    Add speed modifiers when sprinting or sneaking

--- src/main/java/com/alan199921/astral/flight/FlightHandler.java
+++ src/main/java/com/alan199921/astral/flight/FlightHandler.java
@@ -15,19 +15,19 @@ public class FlightHandler {
     public static void handleAstralFlight(PlayerEntity player) {
         //Gets closest block under player
         int closestY = getClosestBlockUnderPlayer(player);
-
+        MovementType movementType = determineMovementType(player);
         //Move player based on kets pressed
         if (InputHandler.isHoldingForwards(player)) {
-            player.moveRelative(1, new Vec3d(0, 0, calculateSpeedForward(player.posY, closestY)));
+            player.moveRelative(1, new Vec3d(0, 0, calculateSpeedForward(player.posY, closestY, movementType)));
         }
         if (InputHandler.isHoldingBackwards(player)) {
-            player.moveRelative(1, new Vec3d(0, 0, -calculateSpeedForward(player.posY, closestY) * 0.8F));
+            player.moveRelative(1, new Vec3d(0, 0, -calculateSpeedForward(player.posY, closestY, movementType) * 0.8F));
         }
         if (InputHandler.isHoldingLeft(player)) {
-            player.moveRelative(1, new Vec3d(calculateSpeedForward(player.posY, closestY), 0, 0));
+            player.moveRelative(1, new Vec3d(calculateSpeedForward(player.posY, closestY, movementType), 0, 0));
         }
         if (InputHandler.isHoldingRight(player)) {
-            player.moveRelative(1, new Vec3d(-calculateSpeedForward(player.posY, closestY), 0, 0));
+            player.moveRelative(1, new Vec3d(-calculateSpeedForward(player.posY, closestY, movementType), 0, 0));
         }
 
         if (InputHandler.isHoldingUp(player)) {
@@ -48,6 +48,43 @@ public class FlightHandler {
         }
     }
 
+    private static MovementType determineMovementType(PlayerEntity player) {
+        if (player.isSneaking()) {
+            return MovementType.SNEAKING;
+        }
+        else if (player.isSprinting()) {
+            return MovementType.SPRINTING;
+        }
+        else {
+            return MovementType.WALKING;
+        }
+    }
+
+    /**
+     * Calculates the speed of the player based on their height and the height of the closest block underneath them
+     *
+     * @param posY         The player's height
+     * @param closestBlock The height of the closest block under the player
+     * @param movementType How the player is moving (sprinting, walking, sneaking)
+     * @return The speed that the player will travel at, in blocks/tick
+     */
+    private static double calculateSpeedForward(double posY, int closestBlock, MovementType movementType) {
+        double baseSpeed = AstralConfig.getFlightSettings().getBaseSpeed() / 80;
+        if (movementType == MovementType.SNEAKING) {
+            return 0.0;
+        }
+        else if (movementType == MovementType.SPRINTING) {
+            baseSpeed *= 1.3;
+        }
+        double speedMultiplier = AstralConfig.getFlightSettings().getMaxMultiplier();
+        double maxChange = AstralConfig.getFlightSettings().getMaxPenalty();
+
+        int maxDifference = AstralConfig.getFlightSettings().getHeightPenaltyLimit();
+        double difference = Math.min(maxDifference, posY - closestBlock);
+        double speedPenalty = (difference / maxDifference) * maxChange;
+        return baseSpeed * (speedMultiplier - speedPenalty);
+    }
+
     /**
      * Gets the Y coordinate closest block under the player
      *
@@ -62,21 +99,7 @@ public class FlightHandler {
         return pos.getY();
     }
 
-    /**
-     * Calculates the speed of the player based on their height and the height of the closest block underneath them
-     *
-     * @param posY         The player's height
-     * @param closestBlock The height of the closest block under the player
-     * @return The speed that the player will travel at, in blocks/tick
-     */
-    private static double calculateSpeedForward(double posY, int closestBlock) {
-        double baseSpeed = AstralConfig.getFlightSettings().getBaseSpeed() / 20;
-        double speedMultiplier = AstralConfig.getFlightSettings().getMaxMultiplier();
-        double maxChange = AstralConfig.getFlightSettings().getMaxPenalty();
-
-        int maxDifference = AstralConfig.getFlightSettings().getHeightPenaltyLimit();
-        double difference = Math.min(maxDifference, posY - closestBlock);
-        double speedPenalty = (difference / maxDifference) * maxChange;
-        return baseSpeed * (speedMultiplier - speedPenalty);
+    private enum MovementType {
+        SPRINTING, WALKING, SNEAKING
     }
 }
