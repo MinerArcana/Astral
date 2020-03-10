e536ad97ff10be3cc6a145b08585e87c9d8cf6cd
Alan19 <Alan19@users.noreply.github.com>
2020-03-10 12:02:50 -0400

    Insert items directly into player inventory in inner realm

--- src/main/java/com/alan199921/astral/commands/AstralCommands.java
+++ src/main/java/com/alan199921/astral/commands/AstralCommands.java
@@ -1,6 +1,8 @@
 package com.alan199921.astral.commands;
 
 import com.alan199921.astral.api.AstralAPI;
+import com.alan199921.astral.api.psychicinventory.InventoryType;
+import com.alan199921.astral.api.psychicinventory.PsychicInventoryInstance;
 import com.alan199921.astral.util.UtilityFunctions;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
@@ -13,14 +15,13 @@ import net.minecraft.command.arguments.ItemArgument;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TranslationTextComponent;
-import net.minecraftforge.items.IItemHandler;
 
 import java.util.Collection;
-import java.util.stream.Stream;
 
 public class AstralCommands {
 
     public static void registerCommands(CommandDispatcher<CommandSource> commandDispatcher) {
+        //Burn command is structured similarly to vanilla's give command
         final LiteralArgumentBuilder<CommandSource> burnCommand = Commands.literal("burn").requires(commandSource -> commandSource.hasPermissionLevel(2))
                 .then(Commands.argument("targets", EntityArgument.players())
                         .then(Commands.argument("item", ItemArgument.item())
@@ -31,15 +32,33 @@ public class AstralCommands {
         commandDispatcher.register(burnCommand);
     }
 
+    /**
+     * Adds an ItemStack to a player's Inner Realm inventory. If the player is using their Inner Realm inventory, inserts directly into inventory instead
+     *
+     * @param source    The command source
+     * @param targets   The players to insert the ItemStack into
+     * @param itemStack The ItemStack to be inserted
+     * @return The result of the command, 0 in most cases
+     * @throws CommandSyntaxException Thrown when there is an error in the command syntax
+     */
     private static int addItemToInnerRealmInventory(CommandSource source, Collection<ServerPlayerEntity> targets, ItemStack itemStack) throws CommandSyntaxException {
+        ItemStack copy = itemStack.copy();
         AstralAPI.getOverworldPsychicInventory(source.asPlayer().getServerWorld()).ifPresent(iPsychicInventory -> {
-            final Stream<IItemHandler> targetAstralInventories = targets.stream().map(target -> iPsychicInventory.getInventoryOfPlayer(target.getUniqueID()).getInnerRealmMain());
-            targetAstralInventories.forEach(targetAstralInventory -> UtilityFunctions.insertIntoAnySlot(targetAstralInventory, itemStack));
+            targets.forEach(target -> {
+                final PsychicInventoryInstance playerInventory = iPsychicInventory.getInventoryOfPlayer(target.getUniqueID());
+                //Insert directly into player inventory if they are using their inner realm inventory
+                if (playerInventory.getInventoryType() == InventoryType.INNER_REALM) {
+                    target.inventory.addItemStackToInventory(itemStack);
+                }
+                else {
+                    UtilityFunctions.insertIntoAnySlot(playerInventory.getInnerRealmMain(), itemStack);
+                }
+            });
             if (targets.size() == 1) {
-                source.sendFeedback(new TranslationTextComponent("commands.burn.success.single", itemStack.getCount(), itemStack.getTextComponent(), targets.iterator().next().getDisplayName()), true);
+                source.sendFeedback(new TranslationTextComponent("commands.burn.success.single", copy.getCount(), copy.getTextComponent(), targets.iterator().next().getDisplayName()), true);
             }
             else {
-                source.sendFeedback(new TranslationTextComponent("commands.burn.success.multiple", itemStack.getCount(), itemStack.getTextComponent(), targets.size()), true);
+                source.sendFeedback(new TranslationTextComponent("commands.burn.success.multiple", copy.getCount(), copy.getTextComponent(), targets.size()), true);
             }
         });
         return 0;
