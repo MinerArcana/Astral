8fc40c18929cc2150a49bb8b39027d2389c7db4d
Alan19 <Alan19@users.noreply.github.com>
2020-05-06 17:49:13 -0400

    Move Ethereal block behavior inside interface for easier maintainability in the future?

--- src/main/java/com/alan199921/astral/blocks/etherealblocks/Ethereal.java
+++ src/main/java/com/alan199921/astral/blocks/etherealblocks/Ethereal.java
@@ -1,4 +1,69 @@
 package com.alan199921.astral.blocks.etherealblocks;
 
+import com.alan199921.astral.effects.AstralEffects;
+import com.alan199921.astral.tags.AstralTags;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
 public interface Ethereal {
+
+    int ETHEREAL_BLOCK_OPACITY = 0;
+
+    /**
+     * Render the block as invisible if the player does not have Astral Travel
+     *
+     * @param defaultReturn The BlockRenderType returned from the super function of the block
+     * @return Invisible if player does not have Astral Travel, super if player does
+     */
+    static BlockRenderType getRenderType(BlockRenderType defaultReturn) {
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+        if (player != null) {
+            return player.isPotionActive(AstralEffects.ASTRAL_TRAVEL) ? defaultReturn : BlockRenderType.INVISIBLE;
+        }
+        return defaultReturn;
+    }
+
+    static boolean canEntityDestroy(Entity entity, boolean defaultReturn) {
+        if (entity instanceof LivingEntity) {
+            return ((LivingEntity) entity).isPotionActive(AstralEffects.ASTRAL_TRAVEL);
+        }
+        return defaultReturn;
+    }
+
+    /**
+     * Allows Astral entities and items to not pass through Ethereal blocks
+     *
+     * @param context The context of the shape query
+     * @return super (usually a regular shape) if the entity is Astral, empty if not
+     */
+    static VoxelShape getCollisionShape(ISelectionContext context, VoxelShape parentReturn) {
+        if (context.getEntity() instanceof LivingEntity && ((LivingEntity) context.getEntity()).isPotionActive(AstralEffects.ASTRAL_TRAVEL) || context.getEntity() instanceof ItemEntity && AstralTags.ASTRAL_PICKUP.contains(((ItemEntity) context.getEntity()).getItem().getItem())) {
+            return parentReturn;
+        }
+        return VoxelShapes.empty();
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    static VoxelShape getShape(VoxelShape parentShape) {
+        ClientPlayerEntity clientPlayerEntity = Minecraft.getInstance().player;
+        if (clientPlayerEntity != null && !clientPlayerEntity.isPotionActive(AstralEffects.ASTRAL_TRAVEL)) {
+            return VoxelShapes.empty();
+        }
+        return parentShape;
+    }
+
+    static int getOpacity() {
+        return ETHEREAL_BLOCK_OPACITY;
+    }
+
+
 }
--- src/main/java/com/alan199921/astral/blocks/etherealblocks/EtherealDoublePlantBlock.java
+++ src/main/java/com/alan199921/astral/blocks/etherealblocks/EtherealDoublePlantBlock.java
@@ -1,19 +1,11 @@
 package com.alan199921.astral.blocks.etherealblocks;
 
-import com.alan199921.astral.blocks.AstralBlocks;
-import com.alan199921.astral.effects.AstralEffects;
-import com.alan199921.astral.tags.AstralTags;
 import net.minecraft.block.*;
 import net.minecraft.block.material.Material;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
-import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -25,67 +17,32 @@ public class EtherealDoublePlantBlock extends DoublePlantBlock {
         super(Block.Properties.create(Material.TALL_PLANTS).doesNotBlockMovement().hardnessAndResistance(0).sound(SoundType.PLANT));
     }
 
-    @Override
-    protected boolean isValidGround(BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos) {
-        return state.getBlock() == AstralBlocks.ETHER_DIRT.get() || state.getBlock() == AstralBlocks.ETHER_GRASS.get() || super.isValidGround(state, worldIn, pos);
-    }
-
-    /**
-     * Render the block as invisible if the player does not have Astral Travel
-     *
-     * @param state The block state
-     * @return Invisible if player does not have Astral Travel, super if player does
-     */
     @Nonnull
     @Override
     public BlockRenderType getRenderType(@Nonnull BlockState state) {
-        ClientPlayerEntity player = Minecraft.getInstance().player;
-        if (player != null) {
-            return player.isPotionActive(AstralEffects.ASTRAL_TRAVEL) ? super.getRenderType(state) : BlockRenderType.INVISIBLE;
-        }
-        return super.getRenderType(state);
+        return Ethereal.getRenderType(super.getRenderType(state));
     }
 
     @Override
     public boolean canEntityDestroy(BlockState state, IBlockReader world, BlockPos pos, Entity entity) {
-        if (entity instanceof LivingEntity) {
-            return ((LivingEntity) entity).isPotionActive(AstralEffects.ASTRAL_TRAVEL);
-        }
-        return super.canEntityDestroy(state, world, pos, entity);
+        return Ethereal.canEntityDestroy(entity, super.canEntityDestroy(state, world, pos, entity));
     }
 
-    /**
-     * Allows Astral entities and items to not pass through Ethereal blocks
-     *
-     * @param state   The blockstate of the ethereal block
-     * @param worldIn The world
-     * @param pos     The blockpos
-     * @param context The context of the shape query
-     * @return super (usually a regular shape) if the entity is Astral, empty if not
-     */
     @Nonnull
     @Override
-    public VoxelShape getCollisionShape(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos, ISelectionContext context) {
-        if (context.getEntity() instanceof LivingEntity && ((LivingEntity) context.getEntity()).isPotionActive(AstralEffects.ASTRAL_TRAVEL) || context.getEntity() instanceof ItemEntity && AstralTags.ASTRAL_PICKUP.contains(((ItemEntity) context.getEntity()).getItem().getItem())) {
-            return super.getCollisionShape(state, worldIn, pos, context);
-        }
-        return VoxelShapes.empty();
+    public VoxelShape getCollisionShape(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos, @Nonnull ISelectionContext context) {
+        return Ethereal.getCollisionShape(context, super.getCollisionShape(state, worldIn, pos, context));
     }
 
     @OnlyIn(Dist.CLIENT)
     @Nonnull
     @Override
     public VoxelShape getShape(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos, @Nonnull ISelectionContext context) {
-        ClientPlayerEntity clientPlayerEntity = Minecraft.getInstance().player;
-        if (clientPlayerEntity != null && !clientPlayerEntity.isPotionActive(AstralEffects.ASTRAL_TRAVEL)) {
-            return VoxelShapes.empty();
-        }
-        return super.getShape(state, worldIn, pos, context);
+        return Ethereal.getShape(super.getShape(state, worldIn, pos, context));
     }
 
     @Override
     public int getOpacity(@Nonnull BlockState state, @Nonnull IBlockReader worldIn, @Nonnull BlockPos pos) {
-        return 0;
+        return Ethereal.getOpacity();
     }
-
 }
