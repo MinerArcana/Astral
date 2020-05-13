0a3a312354582144ad17fa62452aa8da7fdacee7
Alan19 <Alan19@users.noreply.github.com>
2020-05-12 23:38:16 -0400

    Change structure of mental construct Hashmap to String, Pair<MentalConstruct, Integer>

--- src/main/java/com/alan199921/astral/mentalconstructs/PlayerMentalConstructTracker.java
+++ src/main/java/com/alan199921/astral/mentalconstructs/PlayerMentalConstructTracker.java
@@ -1,26 +1,41 @@
 package com.alan199921.astral.mentalconstructs;
 
+import javafx.util.Pair;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.common.util.INBTSerializable;
+import net.minecraftforge.fml.RegistryObject;
 
 import java.util.HashMap;
 
 public class PlayerMentalConstructTracker implements INBTSerializable<CompoundNBT> {
-    private final HashMap<MentalConstruct, Integer> mentalConstructs = new HashMap<>();
+    private final HashMap<String, Pair<MentalConstruct, Integer>> mentalConstructs = new HashMap<>();
 
     public PlayerMentalConstructTracker() {
-        AstralMentalConstructs.MENTAL_CONSTRUCTS.getEntries().forEach(mentalConstruct -> mentalConstructs.put(mentalConstruct.get().create(), -1));
+        for (RegistryObject<MentalConstructType> mentalConstruct : AstralMentalConstructs.MENTAL_CONSTRUCTS.getEntries()) {
+            mentalConstructs.put(mentalConstruct.getId().getPath(), new Pair<>(mentalConstruct.get().create(), -1));
+        }
     }
 
     @Override
     public CompoundNBT serializeNBT() {
         CompoundNBT nbt = new CompoundNBT();
-        mentalConstructs.forEach((key, value) -> nbt.putInt(key.getRegistryName().getPath(), value));
+        mentalConstructs.forEach((key, value) -> {
+            final CompoundNBT constructNBT = new CompoundNBT();
+            constructNBT.put("constructInfo", value.getKey().serializeNBT());
+            constructNBT.putInt("level", value.getValue());
+            nbt.put(key, constructNBT);
+        });
         return nbt;
     }
 
     @Override
     public void deserializeNBT(CompoundNBT nbt) {
-        AstralMentalConstructs.MENTAL_CONSTRUCTS.getEntries().forEach(mentalConstruct -> mentalConstructs.put(mentalConstruct.get().create(), nbt.getInt(mentalConstruct.getId().getPath())));
+        for (RegistryObject<MentalConstructType> mentalConstruct : AstralMentalConstructs.MENTAL_CONSTRUCTS.getEntries()) {
+            final MentalConstruct createdMentalConstruct = mentalConstruct.get().create();
+            CompoundNBT constructNbt = nbt.getCompound(mentalConstruct.getId().getPath());
+            createdMentalConstruct.deserializeNBT(constructNbt.getCompound("constructInfo"));
+            int level = constructNbt.getInt("level");
+            mentalConstructs.put(mentalConstruct.getId().getPath(), new Pair<>(createdMentalConstruct, level));
+        }
     }
 }
