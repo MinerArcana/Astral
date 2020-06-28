ea5271c110055a317ddaab63d29c75ccfeec973f
Alan19 <Alan19@users.noreply.github.com>
2020-06-28 15:16:17 -0400

    Successfully grab Astral inventory

--- src/main/java/com/alan19/astral/client/gui/AstralInventoryContainer.java
+++ src/main/java/com/alan19/astral/client/gui/AstralInventoryContainer.java
@@ -2,6 +2,7 @@ package com.alan19.astral.client.gui;
 
 import com.alan19.astral.api.AstralAPI;
 import com.alan19.astral.api.psychicinventory.PsychicInventory;
+import com.alan19.astral.api.psychicinventory.PsychicInventoryInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -9,22 +10,35 @@ import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.container.RecipeBookContainer;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.RecipeItemHelper;
-import net.minecraftforge.items.ItemStackHandler;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.items.SlotItemHandler;
 
 import javax.annotation.Nonnull;
 
 public class AstralInventoryContainer extends RecipeBookContainer<CraftingInventory> {
     private final PlayerInventory playerInventory;
-    private ItemStackHandler mainInventory = new ItemStackHandler(42);
+    private PsychicInventoryInstance inventory = new PsychicInventoryInstance();
 
-    public AstralInventoryContainer(int i, PlayerInventory playerInventory) {
-        super(AstralContainers.ASTRAL_INVENTORY_CONTAINER.get(), i);
+    public AstralInventoryContainer(int id, PlayerInventory playerInventory) {
+        super(AstralContainers.ASTRAL_INVENTORY_CONTAINER.get(), id);
         this.playerInventory = playerInventory;
         final PlayerEntity player = playerInventory.player;
+//        int xs = 48;
+//        int ys = 143;
+//        for (int row = 0; row < 3; row++) {
+//            for (int col = 0; col < 9; col++) {
+//                addSlot(new Slot(playerInventory, col + row * 9 + 9, xs + col * 18, ys + row * 18));
+//            }
+//        }
+
         if (player instanceof ServerPlayerEntity) {
             if (AstralAPI.getOverworldPsychicInventory(((ServerPlayerEntity) player).getServerWorld()).isPresent()) {
-                mainInventory = AstralAPI.getOverworldPsychicInventory(((ServerPlayerEntity) player).getServerWorld()).orElseGet(PsychicInventory::new).getInventoryOfPlayer(player.getUniqueID()).getPhysicalInventory();
+                inventory = AstralAPI.getOverworldPsychicInventory(((ServerPlayerEntity) player).getServerWorld()).orElseGet(PsychicInventory::new).getInventoryOfPlayer(player.getUniqueID());
             }
+        }
+        for (int slot = 0; slot < inventory.getAstralMainInventory().getSlots(); slot++) {
+            addSlot(new SlotItemHandler(inventory.getAstralMainInventory(), slot, -7, slot * 24));
         }
     }
 
@@ -55,16 +69,17 @@ public class AstralInventoryContainer extends RecipeBookContainer<CraftingInvent
 
     @Override
     public int getWidth() {
-        return 175;
+        return 2;
     }
 
     @Override
     public int getHeight() {
-        return 165;
+        return 2;
     }
 
     @Override
+    @OnlyIn(Dist.CLIENT)
     public int getSize() {
-        return 10;
+        return 5;
     }
 }
