e23678c88beb7a4f4038a31687fc03499b96695e
Alan19 <Alan19@users.noreply.github.com>
2020-06-29 17:52:20 -0400

    Add armor slots for Astral gui

--- src/main/java/com/alan19/astral/client/gui/AstralInventoryContainer.java
+++ src/main/java/com/alan19/astral/client/gui/AstralInventoryContainer.java
@@ -6,10 +6,15 @@ import com.alan19.astral.api.psychicinventory.PsychicInventoryInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.inventory.CraftResultInventory;
 import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.inventory.container.RecipeBookContainer;
+import net.minecraft.inventory.container.Slot;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.RecipeItemHelper;
+import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.items.SlotItemHandler;
@@ -18,12 +23,13 @@ import javax.annotation.Nonnull;
 import java.util.stream.IntStream;
 
 public class AstralInventoryContainer extends RecipeBookContainer<CraftingInventory> {
-    private final PlayerInventory playerInventory;
+    private final CraftingInventory craftMatrix = new CraftingInventory(this, 2, 2);
+    private final CraftResultInventory craftResult = new CraftResultInventory();
     private PsychicInventoryInstance inventory = new PsychicInventoryInstance();
+
 
     public AstralInventoryContainer(int id, PlayerInventory playerInventory) {
         super(AstralContainers.ASTRAL_INVENTORY_CONTAINER.get(), id);
-        this.playerInventory = playerInventory;
         final PlayerEntity player = playerInventory.player;
         if (player instanceof ServerPlayerEntity && AstralAPI.getOverworldPsychicInventory(((ServerPlayerEntity) player).getServerWorld()).isPresent()) {
             inventory = AstralAPI.getOverworldPsychicInventory(((ServerPlayerEntity) player).getServerWorld()).orElseGet(PsychicInventory::new).getInventoryOfPlayer(player.getUniqueID());
@@ -38,6 +44,11 @@ public class AstralInventoryContainer extends RecipeBookContainer<CraftingInvent
         IntStream.range(0, 9)
                 .mapToObj(hotbarSlot -> new SlotItemHandler(inventory.getAstralMainInventory(), hotbarSlot, 8 + hotbarSlot * 18, 142))
                 .forEach(this::addSlot);
+
+        IntStream.range(0, 4)
+                .mapToObj(i -> EquipmentSlotType.fromSlotTypeAndIndex(EquipmentSlotType.Group.ARMOR, i))
+                .map(equipmentSlotType -> new EquipmentSlotHandler(equipmentSlotType, player, inventory.getAstralArmorInventory(), equipmentSlotType.getIndex(), 8, 8 + (3 - equipmentSlotType.getIndex()) * 18))
+                .forEach(this::addSlot);
     }
 
     @Override
@@ -47,12 +58,13 @@ public class AstralInventoryContainer extends RecipeBookContainer<CraftingInvent
 
     @Override
     public void fillStackedContents(@Nonnull RecipeItemHelper itemHelperIn) {
-
+        craftMatrix.fillStackedContents(itemHelperIn);
     }
 
     @Override
     public void clear() {
-
+        this.craftResult.clear();
+        this.craftMatrix.clear();
     }
 
     @Override
@@ -80,4 +92,33 @@ public class AstralInventoryContainer extends RecipeBookContainer<CraftingInvent
     public int getSize() {
         return 5;
     }
+
+    @Override
+    public void onContainerClosed(@Nonnull PlayerEntity playerIn) {
+        super.onContainerClosed(playerIn);
+        this.craftResult.clear();
+        if (playerIn.world instanceof ServerWorld) {
+            this.clearContainer(playerIn, playerIn.world, this.craftMatrix);
+        }
+    }
+
+    /**
+     * Handle when the stack in slot {@code index} is shift-clicked. Normally this moves the stack between the player
+     * inventory and the other inventory(s).
+     */
+    @Override
+    @Nonnull
+    public ItemStack transferStackInSlot(@Nonnull PlayerEntity playerIn, int index) {
+        return ItemStack.EMPTY;
+    }
+
+    /**
+     * Called to determine if the current slot is valid for the stack merging (double-click) code. The stack passed in is
+     * null for the initial slot that was double-clicked.
+     */
+    @Override
+    public boolean canMergeSlot(@Nonnull ItemStack stack, Slot slotIn) {
+        return slotIn.inventory != this.craftResult && super.canMergeSlot(stack, slotIn);
+    }
+
 }
