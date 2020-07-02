91a24e250298464087b5046809519271e1f35d43
Alan19 <Alan19@users.noreply.github.com>
2020-07-01 22:07:59 -0400

    Hide Astral inventory button when you have Astral Travel

--- src/main/java/com/alan19/astral/client/gui/GuiEventHandler.java
+++ src/main/java/com/alan19/astral/client/gui/GuiEventHandler.java
@@ -1,6 +1,8 @@
 package com.alan19.astral.client.gui;
 
 import com.alan19.astral.Astral;
+import com.alan19.astral.effects.AstralEffects;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.gui.screen.inventory.CreativeScreen;
@@ -13,7 +15,7 @@ public class GuiEventHandler {
     @SubscribeEvent
     public static void addWidgetToInventory(GuiScreenEvent.InitGuiEvent.Post event) {
         Screen screen = event.getGui();
-        if (screen instanceof InventoryScreen || screen instanceof CreativeScreen) {
+        if (Minecraft.getInstance().player != null && (screen instanceof InventoryScreen || screen instanceof CreativeScreen && !Minecraft.getInstance().player.isPotionActive(AstralEffects.ASTRAL_TRAVEL.get()))) {
             ContainerScreen<?> gui = (ContainerScreen<?>) screen;
             event.addWidget(new AstralInventoryButton(gui, gui.getGuiLeft() + 150, gui.height / 2 - 22, 20, 18, 0, 0, 19, new ResourceLocation(Astral.MOD_ID, "textures/gui/astral_inventory_button.png")));
         }
