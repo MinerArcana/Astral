eb045bab868a98fba036cce19a01d65658446ca5
Alan19 <Alan19@users.noreply.github.com>
2020-07-04 16:38:33 -0400

    Also check for curios

--- src/main/java/com/alan19/astral/mentalconstructs/Garden.java
+++ src/main/java/com/alan19/astral/mentalconstructs/Garden.java
@@ -5,8 +5,13 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraftforge.fml.common.Mod;
+import top.theillusivec4.curios.api.capability.CuriosCapability;
+import top.theillusivec4.curios.api.capability.ICurioItemHandler;
+import top.theillusivec4.curios.api.inventory.CurioStackHandler;
 import vazkii.botania.api.mana.IManaItem;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,6 +22,17 @@ public class Garden extends MentalConstruct {
 
     public static float getConversionRatio(int level) {
         return 14 / (.002f * level + 1) + 1;
+    }
+
+    private static List<ItemStack> getCuriosAsList(ICurioItemHandler iCurioItemHandler) {
+        final Collection<CurioStackHandler> curioStackHandlers = iCurioItemHandler.getCurioMap().values();
+        List<ItemStack> curios = new ArrayList<>();
+        for (CurioStackHandler curioStackHandler : curioStackHandlers) {
+            for (int i = 0; i < curioStackHandler.getSlots(); i++) {
+                curios.add(curioStackHandler.getStackInSlot(i));
+            }
+        }
+        return curios;
     }
 
     @Override
@@ -47,7 +63,9 @@ public class Garden extends MentalConstruct {
 
 
     private List<ItemStack> getManaItems(PlayerEntity player) {
-        return player.inventory.mainInventory.stream().filter(itemStack -> itemStack.getItem() instanceof IManaItem).collect(Collectors.toList());
+        final List<ItemStack> curiosItems = player.getCapability(CuriosCapability.INVENTORY).map(Garden::getCuriosAsList).orElseGet(ArrayList::new);
+        curiosItems.addAll(player.inventory.mainInventory);
+        return curiosItems.stream().filter(itemStack -> itemStack.getItem() instanceof IManaItem).collect(Collectors.toList());
     }
 
     @Override
