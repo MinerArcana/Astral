eb7443c9df7ddb2fd3e731eb3c0bcb944c82c51a
Alan19 <Alan19@users.noreply.github.com>
2020-07-10 17:27:48 -0400

    Add Etheric Powder as an ingredient for Extrospection Brew

--- src/main/java/com/alan19/astral/data/providers/AstralBrewProvider.java
+++ src/main/java/com/alan19/astral/data/providers/AstralBrewProvider.java
@@ -27,7 +27,7 @@ public class AstralBrewProvider extends BrewProvider {
 
     @Override
     protected void registerRecipes(@Nonnull Consumer<IFinishedRecipe> consumer) {
-        createBotanicalBreweryRecipe(consumer, AstralBotaniaBrews.ASTRAL_TRAVEL, Ingredient.fromItems(Items.NETHER_WART), Ingredient.fromItems(AstralItems.TRAVELING_MEDICINE.get()), Ingredient.fromItems(Items.GLOWSTONE_DUST));
+        createBotanicalBreweryRecipe(consumer, AstralBotaniaBrews.ASTRAL_TRAVEL, Ingredient.fromItems(Items.NETHER_WART), Ingredient.fromItems(AstralItems.TRAVELING_MEDICINE.get(), AstralItems.ETHERIC_POWDER_ITEM.get()), Ingredient.fromItems(Items.GLOWSTONE_DUST));
         createBotanicalBreweryRecipe(consumer, AstralBotaniaBrews.SNOWBERRY_BREW, Ingredient.fromItems(Items.REDSTONE), Ingredient.fromItems(AstralItems.SNOWBERRY.get()), Ingredient.fromItems(Items.GLOWSTONE_DUST));
         createBotanicalBreweryRecipe(consumer, AstralBotaniaBrews.FEVERWEED_BREW, Ingredient.fromItems(Items.REDSTONE), Ingredient.fromItems(AstralItems.FEVERWEED.get()), Ingredient.fromItems(Items.GLOWSTONE_DUST));
     }
