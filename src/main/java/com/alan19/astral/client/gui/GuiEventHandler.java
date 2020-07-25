0f26cab8c823a0cc0b87d9677344d6dbd54ffdf2
Alan19 <Alan19@users.noreply.github.com>
2020-07-24 22:51:06 -0400

    Remove inventory button in creative inventory

--- src/main/java/com/alan19/astral/client/gui/GuiEventHandler.java
+++ src/main/java/com/alan19/astral/client/gui/GuiEventHandler.java
@@ -5,7 +5,6 @@ import com.alan19.astral.effects.AstralEffects;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
-import net.minecraft.client.gui.screen.inventory.CreativeScreen;
 import net.minecraft.client.gui.screen.inventory.InventoryScreen;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.GuiScreenEvent;
@@ -15,7 +14,7 @@ public class GuiEventHandler {
     @SubscribeEvent
     public static void addWidgetToInventory(GuiScreenEvent.InitGuiEvent.Post event) {
         Screen screen = event.getGui();
-        if (Minecraft.getInstance().player != null && !Minecraft.getInstance().player.isPotionActive(AstralEffects.ASTRAL_TRAVEL.get()) && (screen instanceof InventoryScreen || screen instanceof CreativeScreen)) {
+        if (Minecraft.getInstance().player != null && !Minecraft.getInstance().player.isPotionActive(AstralEffects.ASTRAL_TRAVEL.get()) && (screen instanceof InventoryScreen)) {
             ContainerScreen<?> gui = (ContainerScreen<?>) screen;
             event.addWidget(new AstralInventoryButton(gui, gui.getGuiLeft() + 150, gui.height / 2 - 22, 20, 18, 0, 0, 19, new ResourceLocation(Astral.MOD_ID, "textures/gui/astral_inventory_button.png")));
         }
