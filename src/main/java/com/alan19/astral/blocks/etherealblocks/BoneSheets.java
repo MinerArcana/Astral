359e36a9337935c3385c2d1258fc281adcead378
Alan19 <Alan19@users.noreply.github.com>
2020-08-21 12:05:44 -0400

    Add harvest level and tool type to bone sheets and metaphoric stone

--- src/main/java/com/alan19/astral/blocks/etherealblocks/BoneSheets.java
+++ src/main/java/com/alan19/astral/blocks/etherealblocks/BoneSheets.java
@@ -2,9 +2,10 @@ package com.alan19.astral.blocks.etherealblocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
 
 public class BoneSheets extends EtherealBlock implements Ethereal {
     public BoneSheets() {
-        super(Block.Properties.create(Material.ROCK).hardnessAndResistance(2.0F, 6.0F).notSolid());
+        super(Block.Properties.create(Material.ROCK).harvestTool(ToolType.PICKAXE).harvestLevel(1).hardnessAndResistance(2.0F, 6.0F).notSolid());
     }
 }
