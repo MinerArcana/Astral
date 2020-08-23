d881bac4fb7f8307b2f87b382e1985b1278490c3
Alan19 <Alan19@users.noreply.github.com>
2020-08-23 11:56:09 -0400

    Move intention tracker package to events, bump version number

--- src/main/java/com/alan19/astral/events/intentiontracker/IntentionTracker.java
+++ src/main/java/com/alan19/astral/events/intentiontracker/IntentionTracker.java
@@ -1,4 +1,4 @@
-package com.alan19.astral.intentiontracker;
+package com.alan19.astral.events.intentiontracker;
 
 import com.alan19.astral.Astral;
 import com.alan19.astral.ClientSetup;
@@ -6,7 +6,6 @@ import com.alan19.astral.network.AstralNetwork;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
