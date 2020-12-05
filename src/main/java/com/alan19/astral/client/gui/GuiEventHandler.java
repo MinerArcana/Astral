c07e5327b08b53a8dac9e5ea44f6390b6f6a1499
Alan19 <Alan19@users.noreply.github.com>
2020-12-05 17:11:24 -0500

    Fix astral meridians not generating in certain directions, have Astral Meridians only check main hand for empty item, add localization and recipe for brews, move astral inventory button

--- src/main/java/com/alan19/astral/client/gui/GuiEventHandler.java
+++ src/main/java/com/alan19/astral/client/gui/GuiEventHandler.java
@@ -2,6 +2,8 @@ package com.alan19.astral.client.gui;
 
 import com.alan19.astral.Astral;
 import com.alan19.astral.effects.AstralEffects;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementProgress;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
@@ -16,7 +18,12 @@ public class GuiEventHandler {
         Screen screen = event.getGui();
         if (Minecraft.getInstance().player != null && !Minecraft.getInstance().player.isPotionActive(AstralEffects.ASTRAL_TRAVEL.get()) && (screen instanceof InventoryScreen)) {
             ContainerScreen<?> gui = (ContainerScreen<?>) screen;
-            event.addWidget(new AstralInventoryButton(gui, gui.getGuiLeft() + 150, gui.height / 2 - 22, 20, 18, 0, 0, 19, new ResourceLocation(Astral.MOD_ID, "textures/gui/astral_inventory_button.png")));
+            final AstralInventoryButton button = new AstralInventoryButton(gui, gui.getGuiLeft() + 27, gui.height / 2 - 17, 9, 9, 0, 0, 9, new ResourceLocation(Astral.MOD_ID, "textures/gui/astral_inventory_button.png"));
+            event.addWidget(button);
+
+            final Advancement stepsOnTheWind = Minecraft.getInstance().player.connection.getAdvancementManager().getAdvancementList().getAdvancement(new ResourceLocation(Astral.MOD_ID, "steps_on_the_wind"));
+            final AdvancementProgress stepsOnTheWindProgress = Minecraft.getInstance().player.connection.getAdvancementManager().advancementToProgress.get(stepsOnTheWind);
+            button.visible = stepsOnTheWindProgress.isDone();
         }
     }
 }
