7f07ce1022693dcda4f22ae8dfc127bf91d6f6a8
Alan19 <Alan19@users.noreply.github.com>
2020-12-06 21:26:53 -0500

    Add features to Etheric Isles, recolor Astral Inventory button

--- src/main/java/com/alan19/astral/world/AstralConfiguredFeatures.java
+++ src/main/java/com/alan19/astral/world/AstralConfiguredFeatures.java
@@ -4,6 +4,7 @@ import com.alan19.astral.Astral;
 import com.alan19.astral.configs.AstralConfig;
 import com.alan19.astral.world.features.FeverweedFeatureConfig;
 import com.alan19.astral.world.features.SnowberryFeatureConfig;
+import com.alan19.astral.world.trees.EtherealTree;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.WorldGenRegistries;
@@ -19,6 +20,7 @@ public class AstralConfiguredFeatures {
     // Configured features
     public static final Supplier<ConfiguredFeature<?, ?>> CONFIGURED_FEVERWEED = () -> AstralFeatures.FEVERWEED_FEATURE.get().withConfiguration(new FeverweedFeatureConfig(worldgenSettings.getFeverweedMinPatchSize(), worldgenSettings.getFeverweedMaxPatchSize(), worldgenSettings.getFeverweedPatchSpawnRate(), worldgenSettings.getFeverweedPatchSpawnRate(), worldgenSettings.getFeverweedPatchDistribution()));
     public static final Supplier<ConfiguredFeature<?, ?>> CONFIGURED_SNOWBERRY = () -> AstralFeatures.SNOWBERRY_FEATURE.get().withConfiguration(new SnowberryFeatureConfig(worldgenSettings.getSnowberryMinPatchSize(), worldgenSettings.getSnowberryMaxPatchSize(), worldgenSettings.getSnowberryPatchSpawnRate(), 60));
+    public static final Supplier<ConfiguredFeature<?, ?>> CONFIGURED_ETHEREAL_TREE = () -> AstralFeatures.ETHEREAL_TREE.get().withConfiguration(EtherealTree.ETHEREAL_TREE_CONFIG.get()).square();
 
     // Configured structures
     public static final Supplier<StructureFeature<?, ?>> CONFIGURED_ETHERIC_ISLE = () -> AstralStructures.ETHERIC_ISLE.get().withConfiguration(IFeatureConfig.NO_FEATURE_CONFIG);
@@ -29,6 +31,7 @@ public class AstralConfiguredFeatures {
 
         Registry.register(featureRegistry, new ResourceLocation(Astral.MOD_ID, "feverweed"), CONFIGURED_FEVERWEED.get());
         Registry.register(featureRegistry, new ResourceLocation(Astral.MOD_ID, "snowberry"), CONFIGURED_SNOWBERRY.get());
+        Registry.register(featureRegistry, new ResourceLocation(Astral.MOD_ID, "ethereal_tree"), CONFIGURED_ETHEREAL_TREE.get());
 
         Registry.register(structureRegistry, new ResourceLocation(Astral.MOD_ID, "etheric_isle"), CONFIGURED_ETHERIC_ISLE.get());
     }
